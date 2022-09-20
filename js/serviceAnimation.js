467a8c5b3290dc9b6e8d5edb6b89b538f2ffadad
Marc Becker <maarc1@gmx.de>
2022-09-20 09:27:11 +0200

    javascript structured, services animation padding deleted

---
+++ js/serviceAnimation.js
@@ -0,0 +1,21 @@
+export function serviceAnimation (){
+
+const cards = document.querySelectorAll('[data-js="card"]');
+const cardContents = document.querySelectorAll('[data-js="card-content"]');
+const cardArrows = document.querySelectorAll('[data-js="card-arrow"]');
+
+console.log(cards);
+console.log(cardContents);
+console.log(cardArrows);
+
+
+
+for (const key in cards) {
+    cards[key].addEventListener('click', event => {
+        cardContents[key].classList.toggle("card__content--clicked");
+        cardArrows[key].classList.toggle("card__arrow--clicked");
+    })
+}
+    
+}
+
